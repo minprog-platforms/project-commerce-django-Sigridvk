c12606a29babaabcf6670dbe9d648a0cf494c702
Sigrid <sigridvanklaveren@hotmail.com>
2022-05-10 17:26:33 +0200

    tried to make my first models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from numpy import minimum
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=400)
+    starting_bid = models.FloatField(
+        validators=MinValueValidator(0.0)
+    )
+    categ = models.CharField(max_length=64)
+
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
     pass
